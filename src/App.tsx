a625d687c3b509a1e2c39a389150a9f3e64d01d0
John Zhou <itszhoudoh@gmail.com>
2024-02-07 19:29:03 -0800

    first commit:

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+let name: string = "John";
+let age: number;
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
