c4a5a3ced0184f65d32b7707b568443e4f4c4b5b
John Zhou <itszhoudoh@gmail.com>
2024-02-07 22:11:54 -0800

    boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
-
-let name: string = "John";
-let age: number;
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [searchInput, setSearchInput] = useState('');
   return (
     <div className="App">
-      Hello World
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder='Search For Artist'
+            type='input'
+            onKeyDown={ event => {
+              if (event.key == "Enter") {
+                console.log("Pressed Enter");
+              }
+            }}
+            onChange={event => setSearchInput(event?.target.value)}
+          />
+          <Button onClick={() => {console.log("Clicked Button")}}>
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
     </div>
   );
 }
