a34f8d51bfd5d9955cf2ae69bb0204b20179014f
John Zhou <itszhoudoh@gmail.com>
2024-03-18 14:19:29 -0700

    created new object type so i can reference identical nodes inbetween both maps, in the event i delete from endMapKey, I want startMapKey to delete as well

--- src/components/Optimizer.tsx
+++ src/components/Optimizer.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect, Children } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
-
-interface Prop {
-    inputData: { [key: string]: JSX.Element[] };
-}
+import { start } from 'repl';
 
 // const averaging = ((arrays: any) => {
 //     // 3 is number of arrays to be averaged
@@ -45,42 +42,82 @@ const Optimizer: React.FC <any> = ({inputData}) => {
     // },[inputData])
     // console.log(avgData)
 
-    useEffect(() => {
-        const startKeyMap = new Map<number, string[]>([
-            [0, []], //corresponds to C
-            [1, []], //corresponds to C#
-            [2, []], //corresponds to D
-            [3, []], //corresponds to D#
-            [4, []], //corresponds to E
-            [5, []], //corresponds to F
-            [6, []], //corresponds to F#
-            [7, []], //corresponds to G
-            [8, []], //corresponds to G#
-            [9, []], //corresponds to A
-            [10, []], //corresponds to A#
-            [11, []], //corresponds to B
-        ])
-        const endKeyMap = new Map<number, string[]>([
-            [0, []], //corresponds to C
-            [1, []], //corresponds to C#
-            [2, []], //corresponds to D
-            [3, []], //corresponds to D#
-            [4, []], //corresponds to E
-            [5, []], //corresponds to F
-            [6, []], //corresponds to F#
-            [7, []], //corresponds to G
-            [8, []], //corresponds to G#
-            [9, []], //corresponds to A
-            [10, []], //corresponds to A#
-            [11, []], //corresponds to B
-        ])
-        for(const i of inputData) {
-            //Object.keys(i)[0] grabs the id of the data structure
-            endKeyMap.get(largestOfThePack(i[Object.keys(i)[0]].end[0]))?.push(Object.keys(i)[0])
-            startKeyMap.get(largestOfThePack(i[Object.keys(i)[0]].start[0]))?.push(i[Object.keys(i)[0]])
-            console.log(endKeyMap)
-        }
+    interface song {
+        id: string
+        start: number[]
+        end: number[]
+        sameSongLink?: song
+    }
+    const [newData, setNewData] = useState<song[]>([])
+    useEffect (() => {
+        const reformatting = inputData.map((songID: any) => {
+            const newSong: song = {
+                id: Object.keys(songID)[0],
+                start: songID[Object.keys(songID)[0]].start[0],
+                end: songID[Object.keys(songID)[0]].end[0],
+            }
+            return newSong
+        })
+        setNewData(reformatting)   
     },[inputData])
+    useEffect(() => {
+        const startKeyMap = new Map<number, song[]>([
+            [0, []], //corresponds to C
+            [1, []], //corresponds to C#
+            [2, []], //corresponds to D
+            [3, []], //corresponds to D#
+            [4, []], //corresponds to E
+            [5, []], //corresponds to F
+            [6, []], //corresponds to F#
+            [7, []], //corresponds to G
+            [8, []], //corresponds to G#
+            [9, []], //corresponds to A
+            [10, []], //corresponds to A#
+            [11, []], //corresponds to B
+        ])
+        const endKeyMap = new Map<number, song[]>([
+            [0, []], //corresponds to C
+            [1, []], //corresponds to C#
+            [2, []], //corresponds to D
+            [3, []], //corresponds to D#
+            [4, []], //corresponds to E
+            [5, []], //corresponds to F
+            [6, []], //corresponds to F#
+            [7, []], //corresponds to G
+            [8, []], //corresponds to G#
+            [9, []], //corresponds to A
+            [10, []], //corresponds to A#
+            [11, []], //corresponds to B
+        ])
+        // Assign each song to a start and end key
+        for(const i of newData) {
+            const endKey = largestOfThePack(i.end)
+            const startKey = largestOfThePack(i.start)
+            startKeyMap.get(startKey)?.push(i)
+            i.sameSongLink = startKeyMap.get(startKey)![startKeyMap.size - 1]
+            endKeyMap.get(endKey)?.push(i)
+            // Ordering it this way, endKeyMap's song has a reference to startKeyMap's identitcal song
+            // So when I need to delete it, I can delete it from both maps.
+        }
+        // Remove any keys not used in both endKeyMap and startKeyMap
+        for (const key of endKeyMap) {
+            if (key[1].length < 1) {
+                endKeyMap.delete(key[0])
+            }
+        }
+        for (const key of startKeyMap) {
+            if (key[1].length < 1) {
+                startKeyMap.delete(key[0])
+            }
+        }
+        console.log(endKeyMap)
+        console.log(startKeyMap)
+        // Construct new playlist from endKeyMap and startKeyMap
+        const optimizedPlaylist: string[] = [];
+        // while (endKeyMap.size != 0 && startKeyMap.size != 0) {
+
+        // }
+    },[newData])
     // NEXT, use avg data, find max of array, and corresponding key and then group songs by keys:
     // [C, C#, D, D#, E, F, F#, G, G#, A, A# and B] respectively
     return (
